169383f82814cfb2eef4dad79da41b0242aba993
davi <daviforever17@gmail>
2024-10-07 18:19:34 -0300

    desfaiting.

--- src/service/temaFesta/consultarTemaService.js
+++
@@ -1,9 +0,0 @@
-import { consultarTema } from "../../repository/temaRepository.js";
-
-export default async function consultarTemaService(){
-
-    let registros = await consultarTema();
-
-    return registros;
-    
-}
--- src/service/temaFesta/inserirTemaService.js
+++
@@ -1,9 +0,0 @@
-import { inserirTema } from "../../repository/temaRepository.js";
-
-export default async function inserirTemaService(id){
-
-    let idTema = await inserirTema(id);
-
-    return idTema;
-    
-}
