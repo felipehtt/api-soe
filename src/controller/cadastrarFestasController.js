9d8cc6cd11c4f233521f8ae0a6785a414ad4d91b
davi <daviforever17@gmail>
2024-10-24 12:15:56 -0300

    dashBoard

--- src/controller/cadastrarFestasController.js
+++ src/controller/cadastrarFestasController.js
@@ -3,19 +3,59 @@ const endpoints = Router();
 
 import { autenticar } from "../utils/jwt";
 
+import inserirFestaService from "../service/cadastrarFestas/inserirFestaService.js";
+import consultarFestaService from  "../service/cadastrarFestas/consultarFestaService.js"
+import alterarFestaService from "../service/cadastrarFestas/alterarFestaService.js";
+import deletarFestaService from "../service/cadastrarFestas/deletarFestaSevice.js";
+
 endpoints.post('/festa/cadastrar', autenticar, async (req, resp) => {
 
     try {
     
         let festa = req.body;
 
-        let 
+        let id = await inserirFestaService(festa);
+
+        resp.send({
+
+            idFesta: id
+
+        })
+
     }
     catch(err){
-        
+
+        resp.status(400).send({
+            erro: err.message
+        })        
 
     }
 
 })
+
+
+endpoints.get('/festa', autenticar, async (req, resp) => {
+
+    try {
+    
+        let idAdm = req.user.idAdm;
+
+        let registros = await consultarFestaService(idAdm)
+
+        resp.send(registros);
+
+    }
+    catch(err){
+        
+        resp.status(400).send({
+            erro: err.message
+        })
+
+    }
+
+})
+
+
+endpoints.put('/festa', autenticar)
 
 export default endpoints;
