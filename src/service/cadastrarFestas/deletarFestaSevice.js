9d35d5d28d53ac1657f75392aaca591b344bdf3e
davi <daviforever17@gmail>
2024-10-24 12:07:04 -0300

    dash.

--- src/service/cadastrarFestas/deletarFestaSevice.js
+++ src/service/cadastrarFestas/deletarFestaSevice.js
@@ -4,5 +4,10 @@ export default async function deletarFestaService(id){
 
     let linhasAfetadas = await deletarFesta(id);
 
+    if(linhasAfetadas == 0){
+
+        throw new Error('Nenhuma festa deletada.')
+        
+    }
     
 }
